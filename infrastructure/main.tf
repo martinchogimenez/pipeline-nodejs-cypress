545039db54f175b81e670b219cb05acf7b1f96c9
unknown <martinchogimenez@gmail.com>
2025-05-06 15:12:51 -0300

    Adding tf file

---
+++ infrastructure/main.tf
@@ -0,0 +1,57 @@
+# Configuración del proveedor AWS
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Crear un nuevo grupo de seguridad
+resource "aws_security_group" "instance_security_group" {
+  name        = "instance_security_group_users"
+  description = "Security group for Users API EC2 instance"
+}
+
+# Definición de la instancia EC2
+resource "aws_instance" "users_dev_instance" {
+  ami           = "ami-0a3c3a20c09d6f377" # AMI de Amazon Linux
+  instance_type = "t2.micro"
+  key_name      = "PEM_USERS_DEV" # Actualizá esto si tenés otro nombre de clave
+
+  # Asociar la instancia con el grupo de seguridad recién creado
+  vpc_security_group_ids = [aws_security_group.instance_security_group.id]
+
+  tags = {
+    Name = "pipeline-nodejs-api"
+  }
+}
+
+# Reglas de tráfico
+resource "aws_security_group_rule" "ssh_ingress" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.instance_security_group.id
+}
+
+resource "aws_security_group_rule" "https_ingress" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.instance_security_group.id
+}
+
+resource "aws_security_group_rule" "custom_ingress" {
+  type              = "ingress"
+  from_port         = 3000
+  to_port           = 3000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.instance_security_group.id
+}
+
+# Salida IP pública
+output "public_ip_dev" {
+  value = aws_instance.users_dev_instance.public_ip
+}
