5a54594dd7c01a176af83f43c24aa6441e7600c5
unknown <martinchogimenez@gmail.com>
2025-05-08 11:50:31 -0300

    Updating tf file

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,29 +1,61 @@
-# Configuración del proveedor AWS
+# Proveedor AWS
 provider "aws" {
   region = "sa-east-1"
 }
 
-# Crear un nuevo grupo de seguridad
-resource "aws_security_group" "instance_security_group" {
-  name        = "instance_security_group_users"
-  description = "Security group for Users API EC2 instance"
+# VPC principal
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-# Definición de la instancia EC2
-resource "aws_instance" "users_dev_instance" {
-  ami           = "ami-0e992fa13838f3bf6" # AMI de Amazon Linux
-  instance_type = "t2.micro"
-  key_name      = "PEM_USERS_DEV"
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
 
-  # Asociar la instancia con el grupo de seguridad recién creado
-  vpc_security_group_ids = [aws_security_group.instance_security_group.id]
+# Subnet pública
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
 
-  tags = {
-    Name = "pipeline-nodejs-api"
+# Tabla de rutas
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
   }
 }
 
-# Reglas de tráfico
+# Asociación de la tabla de rutas con la subnet
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# Grupo de seguridad
+resource "aws_security_group" "instance_security_group" {
+  name        = "instance_security_group_users"
+  description = "Security group for Users API EC2 instance"
+  vpc_id      = aws_vpc.main.id
+
+  # Regla de salida (egress)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web_sg"
+  }
+}
+
+# Reglas de entrada (ingress)
 resource "aws_security_group_rule" "ssh_ingress" {
   type              = "ingress"
   from_port         = 22
@@ -51,15 +83,16 @@ resource "aws_security_group_rule" "custom_ingress" {
   security_group_id = aws_security_group.instance_security_group.id
 }
 
-# Salida IP pública
-egress {
-    description = "All outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Instancia EC2
+resource "aws_instance" "users_dev_instance" {
+  ami                         = "ami-0e992fa13838f3bf6" # Amazon Linux
+  instance_type               = "t2.micro"
+  key_name                    = "PEM_USERS_DEV"
+  subnet_id                   = aws_subnet.public.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.instance_security_group.id]
 
   tags = {
-    Name = "web_sg"
+    Name = "pipeline-nodejs-api"
   }
+}
